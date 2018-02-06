0b6fc26d1bb00627631eeb441c44d019c4604d8b
Simon Hoyos <shmesa23@gmail.com>
2018-02-06 07:27:49 -0500

    Create input component

---
+++ src/Input.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  border: 1px solid #333;
+  display: block;
+  font-size: 30px;
+  height: 50px;
+  width: 100%;
+
+  &::placeholder {
+    color: green;
+  }
+
+  @media screen and (min-width: 500px) {
+    width: 500px;
+  }
+`;
