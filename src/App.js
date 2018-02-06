05709f3f6908465083d3ba41c9cf30a2e60be28f
Simon Hoyos <shmesa23@gmail.com>
2018-02-06 08:16:24 -0500

    Show input, buttons, and search term

--- src/App.js
+++ src/App.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react';
+import { Input } from './Input';
+import { PrimaryButton, SecondaryButton } from './Button';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: space-around;
+  width: 100%;
+`;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+      search: '',
+    }
+  }
+
   render() {
+    const { text, search } = this.state;
+
     return (
-      <div className="App" />
+      <Container>
+        <div>
+          <Input
+            onChange={this.handleChange}
+            placeholder="Your text goes here"
+            value={text}
+          />
+          <PrimaryButton
+            onClick={this.handleSearchClick}
+          >
+            Search
+          </PrimaryButton>
+          <SecondaryButton
+            onClick={this.handleClearClick}
+          >
+            Clear
+          </SecondaryButton>
+        </div>
+        You're looking for: {search}
+      </Container>
     );
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      text: e.currentTarget.value,
+    })
+  }
+
+  handleClearClick = () => {
+    this.setState({
+      text: '',
+      search: '',
+    })
+  }
+
+  handleSearchClick = () => {
+    this.setState({
+      search: this.state.text,
+      text: '',
+    })
   }
 }
 
